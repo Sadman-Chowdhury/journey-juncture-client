1b62cdbbe4e9670987fde757204b4c226c52c545
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-04-25 23:29:46 +0600

    Login page design added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,45 @@
+import { Link } from "react-router-dom";
+import { IoEye } from "react-icons/io5";
+import { IoEyeOff } from "react-icons/io5";
+import { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { ImGithub } from "react-icons/im";
 
 const Login = () => {
+    const [showPassword, setShowPassword] = useState(false)
     return (
         <div>
-            
+            <div>
+            <h2 className="text-5xl my-10 text-center font-extrabold text-orange-500">Login <span className="text-black">!</span></h2>
+            <div className='flex flex-col-reverse md:flex-col-reverse lg:flex-row shadow-2xl'>
+                <div className="w-full lg:w-3/5 text-center justify-center">
+                <form className="card-body justify-center mt-10">
+                        <div className="form-control">
+                        <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="relative form-control">
+                        <input className="input input-bordered" placeholder="Password" type={showPassword ? "text" : "password"} name="password" id="" required/>
+                        <span className="absolute top-3 right-3" onClick={()=>setShowPassword(!showPassword)}>
+                            {
+                                showPassword ? <IoEyeOff /> : <IoEye />
+                            }
+                            </span><br />
+                        </div>
+                        <div className="form-control mt-6">
+                        <button className="btn border-orange-300 bg-white text-orange-600">Login</button>
+                        </div>
+                    </form>
+                    <div className="flex flex-col justify-center items-center gap-5">
+                        <button className="border w-1/2 inline-flex gap-5 rounded-lg p-4 bg-blue-100 font-bold"><FcGoogle className="text-2xl ml-10"/>Login with Google</button>
+                        <button className="border w-1/2 inline-flex gap-5 rounded-lg p-4 bg-gray-200 font-bold"><ImGithub className="text-2xl ml-10"/>Login with Github</button>
+                    </div>
+                </div>
+                    <div className='w-full lg:w-2/5'>
+                        <img className='w-full h-full md:h-[600px] lg:h-full' src="https://i.ibb.co/J2VWZkt/20944201.jpg" alt="" />
+                    </div>
+                </div>
+                <p className="text-center mt-5">Do not have an account?<Link className="ml-2 text-orange-600 font-bold" to="/register">Register</Link></p>
+        </div>
         </div>
     );
 };
