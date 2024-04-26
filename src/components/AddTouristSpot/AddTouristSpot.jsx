42eb11560f8ebeb2cda373ba953ce56b43385b67
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-04-26 11:27:24 +0600

    Post functionality added for add tourist spot page

--- src/components/AddTouristSpot/AddTouristSpot.jsx
+++ src/components/AddTouristSpot/AddTouristSpot.jsx
@@ -1,33 +1,75 @@
+import Swal from "sweetalert2";
 
 const AddTouristSpot = () => {
+
+    const handleAddTouristSpot = event => {
+        event.preventDefault()
+        const form = event.target
+        const name = form.name.value;
+        const touristSpotName = form.touristSpotName.value;
+        const countryName = form.countryName.value;
+        const location = form.location.value;
+        const shortDescription = form.shortDescription.value;
+        const averageCost = form.averageCost.value;
+        const seasonality = form.seasonality.value;
+        const travelTime = form.travelTime.value;
+        const totalVisitorPerYear = form.totalVisitorPerYear.value;
+        const email = form.email.value;
+        const photo = form.photo.value;
+        const newTouristSpot = {name, touristSpotName, countryName, location, shortDescription, averageCost, seasonality, travelTime, totalVisitorPerYear, email, photo}
+        console.log(newTouristSpot)
+
+        fetch('http://localhost:3000/touristSpot', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newTouristSpot)
+            })
+            .then(res=>res.json)
+            .then(data=>{
+            console.log(data)
+            Swal.fire({
+                title: 'Success!',
+                text: 'New Tourist Spot added successfully',
+                icon: 'success',
+                confirmButtonText: 'Cool'
+              })
+            if(data.insertedId){
+                alert('User added successfully')
+                form.reset()
+                }
+            })
+    }
+
     return (
         <div className="border border-orange-300 p-20 shadow-2xl mt-14 mb-20 rounded-2xl">
         <h2 className="text-5xl text-center font-bold mb-14 text-orange-500">Add a Tourist Spot</h2>
-        <form className="space-y-10">
+        <form onSubmit={handleAddTouristSpot} className="space-y-10">
             <div className="flex flex-col md:flex-row gap-10">
-                <input type="text" placeholder="Spot Name" name="touristSpotName" className="input input-bordered w-full" />
-                <input type="text" placeholder="Country Name" name="countryName" className="input input-bordered w-full" />
+                <input type="text" placeholder="Spot Name" name="touristSpotName" className="input input-bordered w-full" required />
+                <input type="text" placeholder="Country Name" name="countryName" className="input input-bordered w-full" required/>
             </div>
             <div className="flex flex-col md:flex-row gap-10">
-                <input type="text" placeholder="Location" name="location" className="input input-bordered w-full" />
-                <input type="text" placeholder="Short Description" name="shortDescription" className="input input-bordered w-full" />
+                <input type="text" placeholder="Location" name="location" className="input input-bordered w-full" required/>
+                <input type="text" placeholder="Short Description" name="shortDescription" className="input input-bordered w-full" required/>
             </div>
             <div className="flex flex-col md:flex-row gap-10">
-                <input type="text" placeholder="Average Cost" name="averageCost" className="input input-bordered w-full" />
-                <input type="text" placeholder="Seasonality" name="seasonality" className="input input-bordered w-full" />
+                <input type="text" placeholder="Average Cost" name="averageCost" className="input input-bordered w-full" required/>
+                <input type="text" placeholder="Seasonality" name="seasonality" className="input input-bordered w-full" required/>
             </div>
             <div className="flex flex-col md:flex-row gap-10">
-                <input type="text" placeholder="Travel Time" name="travelTime" className="input input-bordered w-full" />
-                <input type="text" placeholder="Total Visitors Per Year" name="totalVisitorPerYear" className="input input-bordered w-full" />
+                <input type="text" placeholder="Travel Time" name="travelTime" className="input input-bordered w-full" required/>
+                <input type="text" placeholder="Total Visitors Per Year" name="totalVisitorPerYear" className="input input-bordered w-full" required/>
             </div>
             <div className="flex flex-col md:flex-row gap-10">
-                <input type="text" placeholder="User Name" name="name" className="input input-bordered w-full" />
-                <input type="text" placeholder="User Email" name="email" className="input input-bordered w-full" />
+                <input type="text" placeholder="User Name" name="name" className="input input-bordered w-full" required/>
+                <input type="text" placeholder="User Email" name="email" className="input input-bordered w-full" required/>
             </div>
             <div className="">
-                <input type="text" placeholder="Image URL" name="photo" className="input input-bordered w-full" />
+                <input type="text" placeholder="Image URL" name="photo" className="input input-bordered w-full" required/>
             </div>
-            <input type="submit" value="Add" className="btn btn-block text-xl font-extrabold bg-orange-500 border-none"/>
+            <input type="submit" value="Add" className="btn btn-block text-xl font-extrabold bg-orange-500 border-none" required/>
         </form>
     </div>
     );
