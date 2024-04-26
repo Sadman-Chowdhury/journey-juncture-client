ea054f7934170849a15696ddca68673b926265c8
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-04-26 16:36:49 +0600

    Tourist spot section added in home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,16 @@
+import { useLoaderData } from "react-router-dom";
+import { IoLocationSharp } from "react-icons/io5";
+import './Home.css'
 
 const Home = () => {
+
+    const loadedTouristSpot = useLoaderData()
+
+
     return (
         <div className="">
-            <div className="carousel w-full h-[300px] md:h-[500px] lg:h-[700px] shadow-2xl rounded-2xl mt-10 border">
+            {/* Slider */}
+            <div className="carousel w-full h-[400px] md:h-[500px] lg:h-[700px] shadow-2xl rounded-2xl mt-10 border">
             <div id="slide1" className="carousel-item relative w-full">
                 <img src="https://i.ibb.co/sJfsM8w/il-vagabiondo-8e-Nt0-CQam-B0-unsplash.jpg" className="w-full" />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -45,6 +53,27 @@ const Home = () => {
                 <a href="#slide1" className="btn btn-square text-orange-600 text-2xl">❯</a>
                 </div>
             </div>
+            </div>
+
+            {/* tourist spots */}
+            <h1 className="text-5xl font-bold text-orange-500 text-center mt-20 mb-10">Tourist <span className="text-black">Spots</span></h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+                {
+                    loadedTouristSpot.map(touristSpot=><div key={touristSpot._id} style={{ backgroundImage: `url(${touristSpot.photo})` }} className="bg-image h-[300px] md:h-[350px] lg:h-[400px] rounded-2xl flex place-items-end justify-center">
+                        <div className= "py-3 w-3/4">
+                            <div className="p-2 bg-white rounded-2xl flex justify-between gap-5">
+                            <div className="space-y-3">
+                                <h1 className="font-bold text-[18px] text-orange-500">{touristSpot.touristSpotName}</h1>
+                                <div className="flex text-gray-500">
+                                    <IoLocationSharp className="text-2xl"/>
+                                    <p className="">{touristSpot.location}</p>
+                                </div>
+                            </div>
+                            <button className="bg-orange-400 font-semibold text-sm px-2 rounded-2xl text-white">View Details</button>
+                            </div>
+                        </div>
+                    </div>)
+                }
             </div>
         </div>
     );
