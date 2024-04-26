c510a7b59b30ee5c9bf06c673788ace4e9bca792
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-04-27 03:29:16 +0600

    Delete functionality added in my list page

--- src/components/MyList/Mylist.jsx
+++ src/components/MyList/Mylist.jsx
@@ -1,6 +1,7 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
+import Swal from "sweetalert2";
 
 const Mylist = () => {
     const {user} = useContext(AuthContext)
@@ -8,9 +9,40 @@ const Mylist = () => {
     const loggedInUserEmail = user.email;
 
     const filteredAddSpot = userAddedSpot.filter(item => item.email === loggedInUserEmail);
+    const [addedSpots, setAddedSpots] = useState(filteredAddSpot)
 
     // const {_id, touristSpotName, countryName, location, shortDescription, averageCost, seasonality, travelTime, totalVisitorPerYear, photo} = userAddedSpot
 
+    const handleDelete = _id => {
+        console.log(_id)
+        Swal.fire({
+            title: 'Do you want to delete this?',
+            showDenyButton: true,
+            confirmButtonText: 'Yes',
+            denyButtonText: 'No',
+            customClass: {
+              actions: 'my-actions',
+              cancelButton: 'order-1 right-gap',
+              confirmButton: 'order-2',
+              denyButton: 'order-3',
+            },
+          }).then((result) => {
+            if (result.isConfirmed) {
+              fetch(`http://localhost:3000/touristSpot/${_id}`, {
+                    method: 'DELETE'
+                    })
+                    .then(res=>res.json)
+                    .then(data=>{
+                    console.log(data)
+                    Swal.fire('Deleted', '', 'success')
+                })
+                const remaining = addedSpots.filter(spot => spot._id !== _id)
+                setAddedSpots(remaining)
+            } else if (result.isDenied) {
+              Swal.fire('Changes are not saved', '', 'info')
+            }
+          })
+    }
 
     return (
                 <div className="mt-20">
@@ -26,14 +58,14 @@ const Mylist = () => {
                         </tr>
                         </thead>
                         <tbody className="">
-                        {filteredAddSpot.map((spot, idx) => (
+                        {addedSpots.map((spot, idx) => (
                         <tr key={idx} className="text-[18px] font-bold mb-5">
                             <td>{spot.touristSpotName}</td>
                             <td>{spot.countryName}</td>
                             <td>{spot.averageCost}</td>
                             <div className="flex flex-col md:flex-row lg:flex-row gap-2">
                             <Link to={`updateTouristSpot/${spot._id}`}><button className="btn bg-orange-400 text-white">Update</button></Link>
-                            <Link><button className="btn bg-red-500 text-white">Delete</button></Link>
+                            <Link><button onClick={()=>handleDelete(spot._id)} className="btn bg-red-500 text-white">Delete</button></Link>
                             </div>
                         </tr>
                         ))}
